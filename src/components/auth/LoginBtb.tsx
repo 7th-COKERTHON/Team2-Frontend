101ba1a84d07033b0dd1719d4767f0e3cde67ecf
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-24 06:04:44 +0900

    chore: 로그인 관련 코드 삭제

--- src/components/auth/LoginBtb.tsx
+++ src/components/auth/LoginBtb.tsx
@@ -1,21 +1,15 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 import Kakao from "@/assets/kakao.svg";
 
 export function LoginBtn() {
-  const handleKakaoLogin = () => {
-    const REST_API_KEY = process.env.NEXT_PUBLIC_AUTH_KAKAO_ID;
-    const REDIRECT_URI = "http://localhost:3000/login/callback";
-
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
-    window.location.href = kakaoURL;
-  };
-
+  const router = useRouter();
   return (
     <button
       type="button"
-      onClick={handleKakaoLogin}
+      onClick={() => router.push("/")}
       className="bg-yellow flex h-15 w-full items-center justify-center gap-3 rounded-xl p-[10px]"
     >
       <Kakao className="h-5 w-5" />
