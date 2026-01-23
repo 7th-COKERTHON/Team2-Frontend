7dbbc4edaba048b40c8b004d75ef845f3be9afeb
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-23 22:23:18 +0900

    feat: 네비바 컴포넌트 추가

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -1,22 +1,69 @@
+import { useRouter } from "next/navigation";
+
+import { useState } from "react";
+
 import HomeIcon from "@/assets/navbar/home.svg";
 import MypageIcon from "@/assets/navbar/mypage.svg";
 import SearchIcon from "@/assets/navbar/search.svg";
 
+import { NavBarKey } from "@/constants/navBar";
+
 export const NavigationBar = () => {
+  const [activeKey, setActiveKey] = useState<NavBarKey>("home");
+  const router = useRouter();
   return (
-    <nav className="flex w-full items-center gap-25 px-5 pt-[15px] pb-[37px]">
-      <div className="flex flex-col gap-[5px]">
-        <HomeIcon className="h-5 w-5" />
-        <p className="text-cap">홈</p>
-      </div>
-      <div className="flex flex-col gap-[5px]">
-        <SearchIcon className="h-5 w-5" />
-        <p className="text-cap">탐색하기</p>
-      </div>
-      <div className="flex flex-col gap-[5px]">
-        <MypageIcon className="h-5 w-5" />
-        <p className="text-cap">마이페이지</p>
-      </div>
-    </nav>
+    <section className="bg-gray-20 flex w-full items-center justify-center gap-25 rounded-t-[20px] px-5 pt-[15px] pb-[37px]">
+      <button
+        className="flex flex-col items-center gap-[5px]"
+        onClick={() => {
+          setActiveKey("home");
+          router.push("/");
+        }}
+        key="home"
+      >
+        <HomeIcon
+          className={`${activeKey === "home" ? "text-gray-70" : "text-gray-50"} h-5 w-5`}
+        />
+        <p
+          className={`${activeKey === "home" ? "text-gray-70" : "text-gray-50"} text-cap`}
+        >
+          홈
+        </p>
+      </button>
+      <button
+        className="flex flex-col items-center gap-[5px]"
+        onClick={() => {
+          setActiveKey("explore");
+          router.push("/explore");
+        }}
+        key="explore"
+      >
+        <SearchIcon
+          className={`${activeKey === "explore" ? "text-gray-70" : "text-gray-50"} h-5 w-5`}
+        />
+        <p
+          className={`${activeKey === "explore" ? "text-gray-70" : "text-gray-50"} text-cap`}
+        >
+          탐색하기
+        </p>
+      </button>
+      <button
+        className="flex flex-col items-center gap-[5px]"
+        onClick={() => {
+          setActiveKey("mypage");
+          router.push("/mypage");
+        }}
+        key="mypage"
+      >
+        <MypageIcon
+          className={`${activeKey === "mypage" ? "text-gray-70" : "text-gray-50"} h-5 w-5`}
+        />
+        <p
+          className={`${activeKey === "mypage" ? "text-gray-70" : "text-gray-50"} text-cap`}
+        >
+          마이페이지
+        </p>
+      </button>
+    </section>
   );
 };
