ac62eeb3151e5fe7c5e82aed58da5e227360ca2d
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-23 20:41:52 +0900

    chore: 스타일 파일 import 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,4 @@
-import Image from "next/image";
-
-import React from "react";
-
 const Home = () => {
-  return (
-    <>
-      <div className="mt-4 flex w-full items-center justify-between gap-3 rounded-lg border border-gray-300 bg-blue-500 p-2 text-white shadow hover:bg-blue-600">
-        Test
-      </div>
-      <p>홈화면</p>
-    </>
-  );
+  return <p>홈화면</p>;
 };
 export default Home;
