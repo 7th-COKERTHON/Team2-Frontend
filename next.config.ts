b951b799e3cfe5c087da6836638ecc49af0d7bd9
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-23 18:32:20 +0900

    chore: svgr설치

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
+import type { RuleSetRule } from "webpack";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    const rules = config.module?.rules as RuleSetRule[];
+
+    const fileLoaderRule = rules.find(
+      rule => rule.test instanceof RegExp && rule.test.test(".svg"),
+    );
+
+    if (fileLoaderRule) {
+      fileLoaderRule.exclude = /\.svg$/i;
+    }
+
+    rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  turbopack: {},
 };
 
 export default nextConfig;
