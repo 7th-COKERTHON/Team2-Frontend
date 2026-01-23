6c88cfb54a8350937bdf273ee0f92dfa3ee4a8d5
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2026-01-23 19:58:35 +0900

    chore: 폰트시스템 설정 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,22 @@
+import type { Metadata } from "next";
 import localfont from "next/font/local";
 
 // import "@/styles/globals.css";
+export const metadata: Metadata = {
+  title: "찔릿 ZZILLIT",
+  description: "무의식의 나쁜 행동을 바로 잡을 수 있는 웹 서비스",
+  keywords: [
+    "습관",
+    "습관형성",
+    "나쁜습관",
+    "코테이토",
+    "해커톤",
+    "흑과백",
+    "좋은습관",
+    "무의식",
+    "찔릿",
+  ],
+};
 
 const pretendard = localfont({
   src: "../../public/PretendardVariable.woff2",
