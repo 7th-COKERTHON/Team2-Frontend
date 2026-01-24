b6ce06b9501027907a73d89e567c520ce15c69bb
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-24 10:18:06 +0900

    feat: 10자 이하 post제한 message추가

--- src/app/habit/add/page.tsx
+++ src/app/habit/add/page.tsx
@@ -18,6 +18,8 @@ const HabitAdd = () => {
   const [habitText, setHabitText] = useState(""); // 나의 다짐
   const [modalOpen, setModalOpen] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [alertMoment, setAlertMoment] = useState(false);
+  const [alertHabit, setAlertHabit] = useState(false);
 
   const handleClickClose = () => {
     if (momentText.length > 0 || habitText.length > 0) {
@@ -28,6 +30,16 @@ const HabitAdd = () => {
   };
 
   const handleSubmit = async () => {
+    const isMomentInvalid = momentText.length < 10;
+    const isHabitInvalid = habitText.length < 10;
+
+    setAlertMoment(isMomentInvalid);
+    setAlertHabit(isHabitInvalid);
+
+    if (isMomentInvalid || isHabitInvalid) return;
+
+    setLoading(true);
+
     try {
       await postHabit({
         badBehavior: momentText,
@@ -54,39 +66,51 @@ const HabitAdd = () => {
         <h2 className="text-h2 text-gray-100">등록해 보세요</h2>
       </section>
 
-      <section className="mt-[152px] flex flex-col gap-5 px-5">
-        <div className="flex flex-col gap-5">
-          <div className="flex flex-col gap-[5px]">
-            <h3 className="text-h3 text-gray-100">오늘 나의 찔릿한 순간</h3>
-            <p className="text-b3 text-gray-70">
-              무심코 한 잘못된 행동을 돌아보세요
-            </p>
+      <section className="mt-[152px] flex flex-col gap-[17px] px-5">
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-[5px]">
+              <h3 className="text-h3 text-gray-100">오늘 나의 찔릿한 순간</h3>
+              <p className="text-b3 text-gray-70">
+                무심코 한 잘못된 행동을 돌아보세요
+              </p>
+            </div>
+            <div className="w-full rounded-[12px] border border-gray-50 p-5">
+              <textarea
+                value={momentText}
+                onChange={e => setMomentText(e.target.value)}
+                className="text-b4 h-[158px] w-full resize-none text-gray-100 placeholder-gray-50 outline-none"
+                placeholder="예: 엄마가 빨래를 부탁했는데 짜증 내며 거절했다"
+              />
+            </div>
           </div>
-          <div className="w-full rounded-[12px] border border-gray-50 p-5">
-            <textarea
-              value={momentText}
-              onChange={e => setMomentText(e.target.value)}
-              className="text-b4 h-[158px] w-full resize-none text-gray-100 placeholder-gray-50 outline-none"
-              placeholder="예: 엄마가 빨래를 부탁했는데 짜증 내며 거절했다"
-            />
-          </div>
+          {alertMoment && (
+            <p className="text-lab text-pink-100">10자 이상 작성해주세요.</p>
+          )}
         </div>
 
-        <div className="flex flex-col gap-5">
-          <div className="flex flex-col gap-[5px]">
-            <h3 className="text-h3 text-gray-100">나의 다짐</h3>
-            <p className="text-b3 text-gray-70">
-              찔릿했던 나를 위한 다짐을 추가해보세요
-            </p>
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-5">
+            <div className="flex flex-col gap-[5px]">
+              <h3 className="text-h3 text-gray-100">나의 다짐</h3>
+              <p className="text-b3 text-gray-70">
+                찔릿했던 나를 위한 다짐을 추가해보세요
+              </p>
+            </div>
+            <div className="flex flex-col gap-[10px]">
+              <div className="flex h-[57px] w-full items-center rounded-[12px] border border-gray-50 p-5">
+                <textarea
+                  value={habitText}
+                  onChange={e => setHabitText(e.target.value)}
+                  className="text-b4 h-[17px] w-full resize-none text-gray-100 placeholder-gray-50 outline-none"
+                  placeholder="예: 엄마한테 화내기 전 3초 호흡 가다듬기"
+                />
+              </div>
+            </div>
           </div>
-          <div className="flex h-[57px] w-full items-center rounded-[12px] border border-gray-50 p-5">
-            <textarea
-              value={habitText}
-              onChange={e => setHabitText(e.target.value)}
-              className="text-b4 h-[17px] w-full resize-none text-gray-100 placeholder-gray-50 outline-none"
-              placeholder="예: 엄마한테 화내기 전 3초 호흡 가다듬기"
-            />
-          </div>
+          {alertHabit && (
+            <p className="text-lab text-pink-100">10자 이상 작성해주세요.</p>
+          )}
         </div>
       </section>
 
