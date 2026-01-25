9dd72cac5178e504a3a3125c46749b1ced5e76d9
lemoncurdyogurt <suhosinsu1@naver.com>
2026-01-25 21:36:55 +0900

    feat: 네브바 상태로 관리 수정

--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -1,6 +1,5 @@
-import { useRouter } from "next/navigation";
-
-import { useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import HomeIcon from "@/assets/navbar/home.svg";
 import SearchIcon from "@/assets/navbar/search.svg";
@@ -8,44 +7,38 @@ import SearchIcon from "@/assets/navbar/search.svg";
 import { NavBarKey } from "@/constants/navBar";
 
 export const NavigationBar = () => {
-  const [activeKey, setActiveKey] = useState<NavBarKey>("home");
-  const router = useRouter();
+  const pathname = usePathname();
+
+  const isHome = pathname === "/";
+  const isExplore = pathname.startsWith("/explore");
   return (
     <section className="bg-gray-10 z-90 flex w-full items-center justify-center gap-25 rounded-t-[20px] px-5 pt-[15px] pb-[37px]">
-      <button
+      <Link
         className="flex cursor-pointer flex-col items-center gap-[5px]"
-        onClick={() => {
-          setActiveKey("home");
-          router.push("/");
-        }}
+        href={"/"}
         key="home"
       >
         <HomeIcon
-          className={`${activeKey === "home" ? "text-gray-70" : "text-gray-50"} h-5 w-5`}
+          className={`${isHome ? "text-gray-70" : "text-gray-50"} h-5 w-5`}
         />
-        <p
-          className={`${activeKey === "home" ? "text-gray-70" : "text-gray-50"} text-lab`}
-        >
+        <p className={`${isHome ? "text-gray-70" : "text-gray-50"} text-lab`}>
           홈
         </p>
-      </button>
-      <button
+      </Link>
+      <Link
         className="flex cursor-pointer flex-col items-center gap-[5px]"
-        onClick={() => {
-          setActiveKey("explore");
-          router.push("/explore");
-        }}
+        href={"/explore"}
         key="explore"
       >
         <SearchIcon
-          className={`${activeKey === "explore" ? "text-gray-70" : "text-gray-50"} h-5 w-5`}
+          className={`${isExplore ? "text-gray-70" : "text-gray-50"} h-5 w-5`}
         />
         <p
-          className={`${activeKey === "explore" ? "text-gray-70" : "text-gray-50"} text-lab`}
+          className={`${isExplore ? "text-gray-70" : "text-gray-50"} text-lab`}
         >
           탐색하기
         </p>
-      </button>
+      </Link>
     </section>
   );
 };
